33c7e3a11e3eb25977a454695fe5bf78743f61ee
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-06-30 11:26:47 -0500

    Combine quote-generator and refresh-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './styles.css'
-import RefreshButton from '../src/components/refresh-button/refresh-button.jsx'
 import QuoteGenerator from './components/quote-generator/quote-generator'
 import Greeting from './components/greeting/greeting'
 import MoreButton from './components/more-button/more-button'
@@ -8,12 +7,7 @@ export default function App() {
   return (
     <body>
       <div className="background">
-        <div className="upper-container">
-          < QuoteGenerator />
-          <div className="refresh">
-            < RefreshButton />
-          </div>
-        </div>
+        < QuoteGenerator />
         <div className="lower-container">
           <div className="clock-container">
             < Greeting />
--- src/components/quote-generator/quote-generator.jsx
+++ src/components/quote-generator/quote-generator.jsx
@@ -1,12 +1,41 @@
+import React from 'react';
 import './quote-generator.css'
+import { ReactComponent as RefreshIcon } from '../../assets/icons/icon-refresh.svg';
 
 const QuoteGenerator = () => {
+  const [data, setData] = React.useState(null);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch('https://api.quotable.io/random');
+      const { statusCode, statusMessage, ...data } = await response.json();
+      if (!response.ok) throw new Error(`${statusCode} ${statusMessage}`);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+      setData({ content: 'Error fetching quote' });
+    }
+  }
+
+  React.useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  if (!data) return null;
+
   return (
-    <div className="quotes">
-      <p id="quote-body">“The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”</p>
-      <h2 id="author">Ada Lovelace</h2>
+    <div className="upper-container">
+      <div className="quotes">
+        <p id="quote-body">{data.content}</p>
+        <h2 id="author">{data.author}</h2>
+
+      </div>
+      <button className="refresh" onClick={fetchQuote}>
+        <RefreshIcon className="refresh-icon" />
+      </button>
     </div>
   )
 }
+
 
 export default QuoteGenerator
